d83d7d096fd5bad575b7dc3bdccbc05356731474
Austin Garrard <wootasaurus@gmail.com>
2015-08-10 12:48:56 -0500

    added dat.gui

--- assets/algvis.js
+++ assets/algvis.js
@@ -15,6 +15,16 @@ var cGood = '#099b00';
 //algorithm logic
 var input;
 var inputColors;
+var stage;
+var stageLength;
+STAGES = { init:0,
+           showComparison:1, 
+           showComparisonResult:2,
+           showSwap:3 ,
+           done:4};
+var index1;
+var index2;
+var index2;
 
 //other
 var opts;
@@ -25,8 +35,11 @@ function Options() {
     this.color3 = cGood;
     this.inputType = 'ascending';
     this.inputSize = 10;
-    this.sortType = 'selection';
-    
+    this.sortType = ' ';
+    this.reRandomize = function() {
+        this.inputType = 'random';
+        generateInput();
+    };
 }
 
 
@@ -49,18 +62,83 @@ function stopDraw() {
     requestId = 0;
 }
 
-function parseInput(str_data) {
-    var str_array = str_data.trim().split(' ');
-    var int_array = [];
-    for(i = 0; i < str_array.length; i++) {
-        int_array.push(parseInt(str_array[i]));
+function initSort(sort) {
+    if(sort == 'selection') {
+        index1 = 0; //i
+        index2 = 0; //j
+        index3 = 0; //min
+        stage = STAGES.init;
+        setInterval(selectionSort, 1000);
     }
-    return int_array;
 }
 
-function selectionSort(str_data) {
-    var data = parseInput(input.value)
-    
+function selectionSort() {
+    console.log(stage);
+    console.log(index1+' '+index2+' '+index3);
+    for(i = 0; i < opts.inputSize; i++) {
+        inputColors[i] = cDefault;
+    }
+    inputColors[index3] = cGood;
+    if(stage == STAGES.init) {
+        index2 = 1;
+        stage = STAGES.showComparison;
+    }
+
+    else if(stage == STAGES.showComparison) {
+        //inputColors[index3] = cHighlight;
+        inputColors[index2] = cHighlight;
+        stage = STAGES.showComparisonResult;
+    }
+
+    else if(stage == STAGES.showComparisonResult) {
+        if(input[index3] < input[index2]) {
+            inputColors[index3] = cGood;
+            inputColors[index2] = cBad;
+            index2++;
+            if(index2 >= opts.inputSize) {
+                stage = STAGES.showSwap;
+            } else {
+                stage = STAGES.showComparison;
+            }
+        } else {
+            inputColors[index3] = cBad;
+            inputColors[index2] = cGood;
+            index3 = index2;
+            index2++;
+            if(index2 >= opts.inputSize) {
+                stage = STAGES.showSwap;
+            } else {
+                stage = STAGES.showComparison;
+            }
+        }
+        
+    }
+
+    else if(stage == STAGES.showSwap) {
+        var temp = input[index1];
+        input[index1] = input[index3];
+        input[index3] = temp;
+        var temp1 = inputColors[index1];
+        inputColors[index1] = inputColors[index3];
+        inputColors[index3] = temp1;
+
+        index1++;
+        if(index1 == opts.inputSize) {
+            stage = STAGES.done;
+            return;
+        }
+        index2 = index1+1;
+        index3 = index1;
+
+        stage = STAGES.showComparison;
+    }
+
+    else if(stage == STAGES.done) {
+        return;
+    }
+
+
+/*
     for(i = 0; i < data.length; i++) {
         var min = i;
         for(j = i; j < data.length; j++) {
@@ -75,7 +153,7 @@ function selectionSort(str_data) {
             data[i] = temp;
         }
     }
-    testPrint.textContent = data;
+    */
 }
 
 function generateInput() {
@@ -110,6 +188,7 @@ $(document).ready(function(){
     requestId = 0;
     elemWidth = 30;
     elemBaseHeight = 10;
+    stageLength;
     
     //init gui
     opts = new Options();
@@ -124,8 +203,14 @@ $(document).ready(function(){
                 generateInput();
             });
     generateInput();
+    gui.add(opts, 'reRandomize');
     //sort
-    gui.add(opts, 'sortType', ['selection', 'insertion']);
+    gui.add(opts, 'sortType', 
+            [' ', 'selection', 'insertion'])
+            .onFinishChange(function(value) {
+                sortType = value;
+                initSort(value);
+            });
     //debug
     var df = gui.addFolder('debug');
     df.addColor(opts, 'color');
