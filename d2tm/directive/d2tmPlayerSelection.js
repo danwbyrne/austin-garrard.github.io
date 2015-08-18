0f5e4edcc7affdb35fd99c28dd0a038ad133c0ff
Austin Garrard <wootasaurus@gmail.com>
2015-08-18 18:47:02 -0500

    styled player tiles

--- d2tm/directive/d2tmPlayerSelection.js
+++ d2tm/directive/d2tmPlayerSelection.js
@@ -6,18 +6,36 @@ app.directive('d2tmPlayerSelection', function() {
 		scope: {
 			team: '=',	//the roster to add players to
 			players: '=',	//the players to chose from
-			done: '&'		//signal the controller to move to the next screen
+			done: '&',		//signal the controller to move to the next screen
+			options: '='
 		},
 		link: function(scope, element, attrs) {
 
 			scope.addToRoster = function(player) {
-				scope.team.roster.push(player);
-				scope.players.splice(scope.players.indexOf(player), 1);
-			}
+				if(scope.team.roster.length < 6) {
+					scope.team.roster.push(player);
+					scope.players.splice(scope.players.indexOf(player), 1);
+				}
+				else {
+					//display error message
+				}
+			};
 
 			scope.removeFromRoster = function(player) {
 				scope.players.push(player);
 				scope.team.roster.splice(scope.team.roster.indexOf(player), 1);
+				scope.players.sort(function(a,b){ 
+					return a.handle < b.handle ? -1:1; 
+				});
+			};
+
+			scope.setCaptain = function(index) {
+				scope.team.captain = index;
+			};
+
+			scope.pictureUrl = function(player, index) {
+				console.log('http://cdn.dota2.com/apps/dota2/images/heroes/' + player.topHeroes[index]+'_sb.png');
+				return 'http://cdn.dota2.com/apps/dota2/images/heroes/' + player.topHeroes[index]+'_sb.png';
 			}
 		}
 	};
